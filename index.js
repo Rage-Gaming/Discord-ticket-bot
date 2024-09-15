7aaf0c79763a5a22feb552528378036a4ec255d7
Rage Gaming <nidhin12murali@gmail.com>
2024-09-15 19:16:59 +0530

    Added Index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const { Client, GatewayIntentBits } = require("discord.js");
+const commands = require("./commands");
+require('colors');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
+client.on("ready", async () => {
+  console.clear();
+  console.log(`Bot ${client.user.tag} is now online`.green);
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+  (async () => {
+    try {
+      await rest.put(
+        Routes.applicationGuildCommands(
+          process.env.ClientID,
+          process.env.GuildID
+        ),
+        { body: commands }
+      );
+      console.log(`Registered Guild Commands.`.green);
+    } catch (error) {
+      console.error(error);
+    }
+  })();
+
+  require("./ticket")(client);
+});
+
+client.login(process.env.TOKEN);
